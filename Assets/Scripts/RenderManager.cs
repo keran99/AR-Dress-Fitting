2a246b8d258a37b75e693736f4efc17eacab3f63
Keran_ <keran.jegasothy@gmail.com>
2022-09-11 18:33:24 +0200

    add change color and hide avatar

---
+++ Assets/Scripts/RenderManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderManager : MonoBehaviour
+{
+    public Renderer rend;
+
+    public void renderSwitch(){
+        rend = GetComponent<Renderer>();
+        rend.enabled = !rend.enabled;
+    }
+
+    public void renderColor(){
+        Color newColor = new Color( Random.value, Random.value, Random.value, 1.0f );
+        rend = GetComponent<Renderer>();
+        rend.material.color = newColor;
+    }
+}
